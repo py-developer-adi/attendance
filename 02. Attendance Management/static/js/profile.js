c23ab79dff9a85ee4c8e57d48dd41310f24c0dfe
Aditya <adiprofitcoder@gmail.com>
2025-04-06 12:40:13 +0530

    Delete 02. Attendance Management directory

--- 02. Attendance Management/static/js/profile.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".profile .btn").addEventListener('click', () => {
-    document.querySelector(".profile-box").style.display = "none"
-    document.querySelector(".profile").style.display = "none"
-})
